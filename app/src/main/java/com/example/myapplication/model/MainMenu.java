f870ec2f3882dfefcd23cca178eb6f1c54f24fa9
Kemo <ksonko@sfu.ca>
2020-10-11 20:27:18 -0700

    added full screen flag for main menu

--- app/src/main/java/com/example/myapplication/model/MainMenu.java
+++ app/src/main/java/com/example/myapplication/model/MainMenu.java
@@ -3,6 +3,7 @@ package com.example.myapplication.model;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.myapplication.R;
 
@@ -12,5 +13,6 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
