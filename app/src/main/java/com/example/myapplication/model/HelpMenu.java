9b118d9096ab5a78609bec5e779837cf67f8a298
Kemo <ksonko@sfu.ca>
2020-10-19 11:50:58 -0700

    help menu works fine now

--- app/src/main/java/com/example/myapplication/model/HelpMenu.java
+++ app/src/main/java/com/example/myapplication/model/HelpMenu.java
@@ -3,14 +3,27 @@ package com.example.myapplication.model;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
 public class HelpMenu extends AppCompatActivity {
 
+    private Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_menu);
+
+        back = (Button)findViewById(R.id.helpbackbutton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
